7ceecdd30e93256504bb6a4c56c80787fc846ff8
Micah Werbitt <werbitt@gmail.com>
2011-05-10 20:47:38 -0400

    Added a Users controller and layout link tests

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,5 +21,23 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    response.should have_selector('title', :content => "Ruby on Rails")
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+    response.should have_selector('a[href="/"]>img')
+  end
   
 end
