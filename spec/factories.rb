c761859c6ed3a8d4ef0a4314bdb08c2e383ae717
Micah Werbitt <werbitt@gmail.com>
2011-05-15 21:31:11 -0400

    Added user factory and tests for show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Micah Werbitt"
+  user.email                 "werbitt@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
