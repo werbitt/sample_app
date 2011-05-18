355d692f096da44b29f7758832a019cf712d28a0
Micah Werbitt <werbitt@gmail.com>
2011-05-17 21:19:37 -0400

    WOrking user signin

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -17,9 +17,8 @@ describe SessionsController do
   end
 
   describe "Post 'create'" do
-    
-    describe "failure" do
 
+    describe "failure" do
       before(:each) do
         @attr = { :email => "", :password => "" }
       end
@@ -38,10 +37,26 @@ describe SessionsController do
         post :create, :session => @attr
         response.should have_selector('title', :content => "Sign in")
       end
-      
-
-
     end
+      
+    describe "success" do
+      before(:each) do
+        @user = Factory(:user)
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+
+      it "should sign the user in" do
+        post :create, :session => @attr
+        controller.current_user.should == @user
+        controller.should be_signed_in
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end
+
   end
 
 end
